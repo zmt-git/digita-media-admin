af93322777ce213812cf509f0e6f3856de7475f5
ZMT <ZMT819@126.com>
2021-06-25 09:54:53 +0800

    fix upload refresh list

--- src/views/media/index.vue
+++ src/views/media/index.vue
@@ -17,13 +17,15 @@
       >
         <card-media isAddCard>
           <el-upload
-            class="avatar-uploader"
-            action=""
             drag
+            action=""
+            ref="upload"
+            class="avatar-uploader"
             accept='image/bmp, image/jpeg'
+            :auto-upload='false'
             :show-file-list="false"
             :before-upload='beforeUpload'
-            :http-request='uploadMedia'
+            :on-change='onChange'
             :multiple='true'
             :limit='10'
           >
@@ -36,15 +38,15 @@
         <base-page-loading :loading='pageLoading' :noMore='noMore' :list='pageList'></base-page-loading>
       </ul>
     </div>
-    <media-delete-dialog :visible.sync="dialogVisible" @confirm='deleteConfirm'></media-delete-dialog>
+    <!-- <media-delete-dialog :visible.sync="dialogVisible" @confirm='deleteConfirm'></media-delete-dialog> -->
   </div>
 </template>
 <script>
 import CardMedia from '@/components/CardMedia.vue'
 import BasePageLoading from '@/components/BasePageLoading.vue'
-import MediaDeleteDialog from '@/components/MediaDeleteDialog.vue'
+// import MediaDeleteDialog from '@/components/MediaDeleteDialog.vue'
 import { mediaType } from '@/data/common'
-import { getVideoDuration } from '@/utils/tools'
+// import { getVideoDuration } from '@/utils/tools'
 import page from '@/mixins/page'
 import media from '@/mixins/media'
 import { mapGetters } from 'vuex'
@@ -52,7 +54,7 @@ import { listMedia, saveMedia, uploadMedia } from '@/api/media'
 export default {
   name: 'media',
 
-  components: { CardMedia, BasePageLoading, MediaDeleteDialog },
+  components: { CardMedia, BasePageLoading },
 
   computed: {
     ...mapGetters(['user'])
@@ -74,15 +76,26 @@ export default {
         mediaType: -1,
         name: '',
         oldSize: 0
-      }
+      },
+      filelist: [],
+      timer: null
     }
   },
 
   methods: {
     deleteMedia (info) {
-      this.dialogVisible = true
+      // this.dialogVisible = true
       this.targetMedia = info
+      this.$confirm('是否删除媒体？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteConfirm()
+      }).catch(() => {
+      })
     },
+
     // 上传文件前钩子
     beforeUpload (file) {
       const res = mediaType.some(type => {
@@ -93,64 +106,105 @@ export default {
       }
       return res
     },
-    // 上传媒体文件
-    async uploadMedia (config) {
+
+    onChange (file, filelist) {
+      this.filelist = filelist
+      this.timer && clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.uploadMedia()
+      }, 0)
+    },
+
+    async uploadMedia () {
       this.loading = true
-      // 获取存储空间
+
       await this.$store.dispatch('infoUserActions')
-
-      const file = config.file
-
-      const size = file.size / 1024 / 1024
+      const reduce = (accumulator, currentValue) => accumulator + currentValue.raw.size / 1024 / 1024
+      const size = this.filelist.reduce(reduce, 0)
       // 判断空间是否溢满
       if (this.user.storageTotal < this.user.storageUsed + size) {
         this.$message({ type: 'warning', message: '云空间不足，请删除不需要的媒体' })
-        return
       }
+      const promiseMedia = []
+      const promiseInfo = []
+
+      // 上传媒体至媒体服务器
+      this.filelist.forEach(file => {
+        const p = this.createMediaUploadPromise(file.raw)
+        promiseMedia.push(p)
+      })
+
+      await Promise.allSettled(promiseMedia)
+        .then(res => {
+          console.log(res)
+          res.forEach(item => {
+            const p = this.createMediaSavePromise(item.value)
+            promiseInfo.push(p)
+          })
+        })
+        .catch(e => console.log(e))
+
+      // 上传媒体信息
+      await Promise.allSettled(promiseInfo)
+        .catch(e => console.log(e))
+
+      this.$refs.upload.clearFiles()
+      this.filelist = []
+      // 刷新列表
+      this.refresh()
+
+      this.loading = false
+    },
+    // 上传媒体文件
+    createMediaUploadPromise (file) {
+      // 获取存储空间
+
       const formData = new FormData()
 
       formData.append('file', file)
 
-      const res = await this.uploadMediaFile(formData)
-      if (res) {
-        this.getSaveParams(res, file)
-        if (file.type === 'video/mp4') {
-          getVideoDuration(file, async (video) => {
-            this.mediaForm.length = video.duration
-            await this.saveMediaRequest(file)
-            this.refresh()
-          })
-        } else {
-          await this.saveMediaRequest(file)
-          this.refresh()
-        }
-      }
-      this.loading = false
+      return this.uploadMediaFile(formData, file)
     },
+
+    createMediaSavePromise (res) {
+      const mediaForm = this.getSaveParams(res, res.file)
+      return this.saveMediaRequest(res.file, mediaForm)
+    },
+
     // 获取媒体文件请求参数
     getSaveParams (res, file) {
+      const mediaForm = {}
       const type = file.type.split('/').pop()
       const name = (this.user.mobile || 'unknown') + '/' + new Date().getTime() + '.' + type
-      this.mediaForm.name = name
-      this.mediaForm.size = file.size / 1024
-      this.mediaForm.mediaType = mediaType.find(item => item.type === file.type).mediaType
-      this.mediaForm.address = res.url
-      this.mediaForm.length = 10
+      mediaForm.name = name
+      mediaForm.size = file.size / 1024
+      mediaForm.mediaType = mediaType.find(item => item.type === file.type).mediaType
+      mediaForm.address = res.url
+      mediaForm.length = 10
+      return mediaForm
     },
-    uploadMediaFile (formData) {
+
+    uploadMediaFile (formData, file) {
       return uploadMedia(formData)
         .then(res => {
-          return res
+          return { ...res, file: file }
         })
-        .catch(e => console.log(e))
+        .catch(e => {
+          console.log(e)
+          this.$message({ type: 'error', message: `${file.name}上传失败` })
+        })
     },
+
     // 上传媒体文件请求
-    saveMediaRequest (file) {
-      return saveMedia(this.mediaForm)
+    saveMediaRequest (file, mediaForm) {
+      return saveMedia(mediaForm)
         .then(res => {
           this.$message({ type: 'success', message: `${file.name}上传成功` })
         })
-        .catch(e => console.log(e))
+        .catch(e => {
+          console.log(e)
+          this.$message({ type: 'error', message: `${file.name}上传失败` })
+        })
     }
   }
 }
