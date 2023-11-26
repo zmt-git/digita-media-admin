b15d09a43ab38b76d17166e5d4fb2249eca51403
ZMT <zmt_email@163.com>
2023-11-26 19:53:46 +0800

    fix:1126

--- src/api/playList/index.js
+++ src/api/playList/index.js
@@ -1,7 +1,7 @@
 import http from '@/utils/http/axios'
 import Qs from 'qs'
 // 获取播放列表
-export function getPlaylist (devid) {
+export function getPlaylist(devid) {
   return http({
     url: `/device/playlist/list/${devid}`,
     method: 'GET'
@@ -9,30 +9,30 @@ export function getPlaylist (devid) {
 }
 // /device/playlist/update/content
 // 批量修改播放列表内容
-export function updateContent (params) {
+export function updateContent(params) {
   return http({
     url: '/device/playlist/update/content',
     method: 'POST',
     params,
-    paramsSerializer: function (params) {
+    paramsSerializer: function(params) {
       return Qs.stringify(params, { indices: false })
     }
   })
 }
 
-export function updateContentV2 (params) {
+export function updateContentV2(params) {
   return http({
     url: '/device/playlist/update/all',
     method: 'POST',
     params,
-    paramsSerializer: function (params) {
+    paramsSerializer: function(params) {
       return Qs.stringify(params, { indices: false })
     }
   })
 }
 
 // 设置颜色
-export function setColor (id, params) {
+export function setColor(id, params) {
   return http({
     url: `/device/playlist/color/${id}`,
     method: 'POST',
